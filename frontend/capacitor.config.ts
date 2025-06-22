83cdd26fba34f46b8df529625c1f9ca68538aa36
Brendan Yang <antarticuno@gmail.com>
2025-06-22 01:01:51 -0700

    capacitor + capacitor/android + capacitor/ios

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.antarticuno.dmax',
+  appName: 'dynamax-pogo',
+  webDir: 'dist'
+};
+
+export default config;
